885ef1fc1f3a5a67bc285eb786cd8e1d65894b78
James Sorensen <james@ecosleuth.com>
2023-03-08 19:35:15 -0700

    Trimmed unneeded files

--- pl/src/components/layout/MainNavigation.js
+++ pl/src/components/layout/MainNavigation.js
@@ -5,15 +5,13 @@ import classes from './MainNavigation.module.scss';
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}><h2>LabTime - HMI</h2></div>
+      <div className={classes.logo}><h2>Uphold Productions</h2></div>
       <nav>
         <ul>
-          <li key="home"><Link to='/'>Experiment</Link></li>
+          <li key="player"><Link to='/'>Player</Link></li>
+          <li key="config"><Link to='/config'>Configuration</Link></li>
           <li key="mqtt"><Link to='/mqtt'>MQTT</Link></li>
-          <li key="doe"><Link to='/doe'>DOE</Link></li>
           <li key="admin"><Link to='/admin'>Admin</Link></li>
-          <li key="grafana"><Link to='/grafana'>Grafana</Link></li>
-          <li key="play"><Link to='/play'>Play</Link></li>
         </ul>
       </nav>
     </header>
